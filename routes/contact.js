c98594eec51e3559e1db85f3c973c126da8c72c5
maluki65 <brayanmaluki@gmail.com>
2025-10-30 10:23:21 +0300

    Added delete routes

--- routes/contact.js
+++ routes/contact.js
@@ -5,6 +5,7 @@ const { protect } = require('../middlewares/middleware.js');
 
 const router = express.Router();
 
+// On post route
 router.post('/', async(req, res) => {
 
   const contact = new Contact({
@@ -41,6 +42,41 @@ router.get('/', protect, async(req, res) => {
   } catch (error) {
     console.error('Error in getting contacts', error);
     res.status(500).json({ error: 'Failed to fetch contacts'})
+  }
+})
+
+// On getting contact request by id
+router.get('/:id', protect, async(req, res) => {
+  try {
+    const contact = await Contact.findById(req.params.id);
+    if(!contact) return res.status(404).json({
+      error: "Service not found/doesn't exist"
+    })
+    res.json(contact);
+  } catch (error) {
+    console.error('Failed to get contact request:', error)
+    res.status(500).json({
+      error: 'Failed to get contact request'
+    });
+  }
+})
+
+// On deleting contact request
+router.delete('/:id', protect, async(req, res) =>{
+  try {
+    const contact = await Contact.findById(req.params.id);
+    if(!contact) return res.status(404).json({
+      error: "Service not found/doesn't exist"
+    })
+    await Contact.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      message: 'Contact request deleted successufully!'
+    });
+  } catch (error){
+    console.error('Failed to delete contact request:', error);
+    res.status(500).json({
+      message: 'failed to delete contact request'
+    })
   }
 })
 
--- routes/service.js
+++ routes/service.js
@@ -5,6 +5,7 @@ const{ protect } = require('../middlewares/middleware.js');
 
 const router = express.Router();
 
+// On post route
 router.post('/', async(req, res) => {
 
   const service = new Service({
@@ -41,6 +42,42 @@ router.get('/', protect, async(req, res) => {
   } catch (error) {
     console.error('Error in getting service requests', error);
     res.status(500).json({ error: 'Failed to fetch ervice requests'})
+  }
+})
+
+// On getting service by id
+router.get('/:id', protect, async(req, res) => {
+  try {
+    const service = await Service.findById(req.params.id);
+    if (!service) return res.status(404).json({
+      error: "Service not found/doesn't exist"
+    });
+    res.json(service);
+  } catch (error) {
+    res.status(500).json({
+      error: 'Failed to get service request'
+    });
+  }
+})
+
+// On deleting a service request
+router.delete('/:id', protect, async(req, res) => {
+  try {
+    const service = await Service.findById(req.params.id);
+    if(!service) {
+      return res.status(404).json({
+        error: "Service not found/doesn't exist"
+      });
+    }
+    await Service.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      message: 'Service request deleted successfully'
+    });
+  } catch (error) {
+    console.error('Error deleting service request', error);
+    res.status(500).json({
+      message: 'Failedto delete service request'
+    });
   }
 })
 
